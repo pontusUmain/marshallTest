f4cae6826eef36442c79c93eaa8a254bbeee111d
pontusUmain <pontus.croneld@umain.com>
2024-09-13 11:33:53 +0200

    Initial Commit

---
+++ MarshallTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarshallTest
+//
+//  Created by Pontus Croneld on 2024-09-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarshallTest/MarshallTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarshallTestApp.swift
+//  MarshallTest
+//
+//  Created by Pontus Croneld on 2024-09-13.
+//
+
+import SwiftUI
+
+@main
+struct MarshallTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
